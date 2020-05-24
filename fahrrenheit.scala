9168e69107d083332665548e32908df6d2f50f24
Iranga Mudalige <irangapriyam@gmail.com>
2020-05-24 12:14:28 +0530

    initial commit

---
+++ fahrrenheit.scala
@@ -0,0 +1,5 @@
+object cToF extends App {
+  def convertCtoF(c:Double)=c*1.8+32;
+
+  println(convertCtoF(35));	
+}
---
+++ sphere.scala
@@ -0,0 +1,5 @@
+object Main extends App {
+  def volumeSphe(r:Double)=math.Pi*r*r*r*4/3;
+
+  println(volumeSphe(3.0));	
+}
